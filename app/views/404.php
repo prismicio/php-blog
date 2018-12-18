3c318ff9c888a1c53f8d4f464634da5fdf8d5ff7
Levi Gable <levimykel@gmail.com>
2018-12-18 12:27:44 +0100

    Update the 404 page

--- app/views/404.php
+++ app/views/404.php
@@ -1,20 +1,22 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta content="text/html; Charset=UTF-8" http-equiv="Content-Type" />
-    <title>Sample Blog</title>
-    <link rel="stylesheet" href="/stylesheets/reset.css">
-    <link rel="stylesheet" href="/stylesheets/common.css">
-    <link rel="stylesheet" href="/stylesheets/blog.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet" type="text/css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="/images/punch.png" />
-  </head>
-  <body>
-    <div class="blog-main">
-      <h1>Page not found</h1>
-      <p>Sorry we were unable to find the page you are looking for.</p>
-      <p><a href="/" style="text-decoration: underline;">Return to home</a></p>
+<?php
+
+$prismic = $WPGLOBAL['prismic'];
+$isBloghome = false;
+
+?>
+
+<?php include 'header.php'; ?>
+
+<div class="main">
+  <div class="outer-container">
+    <div class="back">
+      <a href="./">back to list</a>
     </div>
-  </body>
-</html>
+
+    <h1>Page not found</h1>
+    <p>Sorry we were unable to find the page you are looking for.</p>
+    <p><a href="/" style="text-decoration: underline;">Return to home</a></p>
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>
