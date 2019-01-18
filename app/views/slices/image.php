8acc97b9e409688d16a275dbecad0ec34c71a9a4
Raul Gonzalez <raulg42@gmail.com>
2019-01-18 13:49:03 +0100

    Update image.php
    Handle slice types in image_with_caption other than just the default

--- app/views/slices/image.php
+++ app/views/slices/image.php
@@ -6,9 +6,34 @@ $imageWithCaption = $slice->primary;
 $imageUrl = $imageWithCaption->image->url;
 $caption = $imageWithCaption->caption;
 
-echo "<div class='post-part single container'><p class='block-img'>";
-echo "<img src='" . $imageUrl . "'/>";
-if ( $caption ) {
-  echo "<p><span class='image-label'>" . RichText::asText($caption) . "</span></p>";
+switch ( $slice->slice_label ) {
+
+  // Full width image
+  case "image-full-width":
+    echo "<div class='blog-header single' style='background-image: url(" . $imageUrl . ");'><div class='wrapper'>";
+    if ( $caption ) {
+      echo "<h1>" . RichText::asText($caption) . "</h1>";
+    }
+    echo "</div></div>";
+    break;  
+
+  // Emphasized image
+  case "emphasized":
+    echo "<div class='post-part single container'><p class='block-img emphasized'>";
+    echo "<img src='" . $imageUrl . "'/>";
+    if ( $caption ) {
+      echo "<p><span class='image-label'>" . RichText::asText($caption) . "</span></p>";
+    }
+    echo "</p></div>";
+    break;  
+
+  // Default image
+  default:
+    echo "<div class='post-part single container'><p class='block-img'>";
+    echo "<img src='" . $imageUrl . "'/>";
+    if ( $caption ) {
+      echo "<p><span class='image-label'>" . RichText::asText($caption) . "</span></p>";
+    }
+    echo "</p></div>";
+    break; 
 }
-echo "</p></div>";
